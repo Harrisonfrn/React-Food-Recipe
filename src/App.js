6cdce0b63aa59529631bd573704d0522e6241a43
HarrisonPro <frnharrison@gmail.com>
2020-07-03 12:50:59 +0200

    feature : featching first data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
   const APP_ID = "324ed399";
   const API_KEY = "c8b94839f15dc98f2e2fdbf2ca647376";
 
-  const exempleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY}`;
+  useEffect(() => {
+    getRecipe();
+  }, []);
+
+  const getRecipe = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY}`
+    );
+    const data = await response.json();
+    console.log(data);
+  };
 
   return (
     <div className="App">
