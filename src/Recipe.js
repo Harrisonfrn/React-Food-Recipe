388e945cb92286b5e48bf04e25cb09535b4f7eaa
HarrisonPro <frnharrison@gmail.com>
2020-07-03 14:50:45 +0200

    finish

--- src/Recipe.js
+++ src/Recipe.js
@@ -10,7 +10,7 @@ const Recipe = ({ title, calories, image, ingredients }) => {
           <li>{ingredients.text}</li>
         ))}
       </ol>
-      <p>{calories}</p>
+      <p>Calories: {calories}</p>
       <img className={style.image} src={image} alt="" />
     </div>
   );
